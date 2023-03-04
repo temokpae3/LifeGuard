b85f7135426944215a4ae309f3c4a061438043be
JirachiLantern <78369596+JirachiLantern@users.noreply.github.com>
2023-03-04 14:00:58 -0500

    Added a peroid
    Kind of unimportant change

--- main.java
+++ main.java
@@ -7,7 +7,7 @@ public class Main {
         String name = scan.next();
         System.out.print("What gender pronouns does " + name +" use?: ");
         String pronoun = scan.next();
-        System.out.println("Hello I am " + name + " my pronouns are " + pronoun);
+        System.out.println("Hello I am " + name + " my pronouns are " + pronoun + ".");
         wordRandomizer myWords = new wordRandomizer();
         for(int i=0; i<20; i++){
             System.out.println(myWords.wordArray().get(i));
