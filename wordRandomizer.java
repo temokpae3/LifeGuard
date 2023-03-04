63faec3aa8913832dfccf98f4943cf30704d1411
JirachiLantern <78369596+JirachiLantern@users.noreply.github.com>
2023-03-04 13:42:24 -0500

    words picked

--- wordRandomizer.java
+++ wordRandomizer.java
@@ -5,24 +5,9 @@ public class wordRandomizer {
 
     public void wordArray(){
         ArrayList<String> words = new ArrayList<>();
-        words.addAll(Arrays.asList("Some Nice Words".split(" ")));
+        words.addAll(Arrays.asList(("algorithm object-oriented fractal concatenate " +
+                "javascript hacking iteration recursion pointer garbage-collection blockchain " +
+                "ubuntu neural-network string instance machine-learning brute-force binary big-endian little-endian").split(" ")));
     }
-
-    //words to add:
-    //algorithm argument arrays object-oriented parameter
-    // assignment operator binary block program
-    // javascript linux declare machine neuron
-    // pointer python script string terminal
-    //variable ubuntu boolean branch character
-    //command concatenate condition constant compiler
-    //exponent instance iteration method pointer
-    //fractal 
-    //lambda-calculas 
-    //abstraction 
-    //brute-force 
-    //garbage-collection 
-    //constructor 
-    //recursion 
-    //hacking
 
 }
