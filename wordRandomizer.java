9b98311bdb428278cddc0ba33a90c86bfa384361
JirachiLantern <78369596+JirachiLantern@users.noreply.github.com>
2023-03-04 13:11:15 -0500

    wordRandomizer add words to choices
    This allows us to add words

--- wordRandomizer.java
+++ wordRandomizer.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class wordRandomizer {
+
+    public void wordArray(){
+        ArrayList<String> words = new ArrayList<>();
+        words.addAll(Arrays.asList("Some Nice Words".split(" ")));
+    }
+
 }
