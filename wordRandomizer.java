e65124d3ae0a2449cf6a871d90183f31264f4ec0
JirachiLantern <78369596+JirachiLantern@users.noreply.github.com>
2023-03-04 13:51:13 -0500

    oops fixed

--- wordRandomizer.java
+++ wordRandomizer.java
@@ -1,17 +1,14 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Name your character: ");
-        String name = scan.next();
-        System.out.print("What gender pronouns does " + name +" use?: ");
-        String pronoun = scan.next();
-        System.out.println("Hello I am " + name + " my pronouns are " + pronoun);
-        wordRandomizer myWords = new wordRandomizer();
-        for(int i=0; i<20; i++){
-            System.out.println(myWords.wordArray().get(i));
-        }
+public class wordRandomizer {
 
+    public ArrayList<String> wordArray(){
+        ArrayList<String> words = new ArrayList<>();
+        words.addAll(Arrays.asList(("algorithm object-oriented fractal concatenate " +
+                "javascript hacking iteration recursion pointer garbage-collection blockchain " +
+                "ubuntu neural-network string instance machine-learning brute-force binary big-endian little-endian").split(" ")));
+        return words;
     }
+
 }
