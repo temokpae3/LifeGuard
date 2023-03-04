458b36e1993d0f01ad981a360becb748d4e47f4e
JirachiLantern <78369596+JirachiLantern@users.noreply.github.com>
2023-03-04 12:54:57 -0500

    Added pronouns
    Added a pronoun option for character

--- main.java
+++ main.java
@@ -4,6 +4,9 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         System.out.print("Name your character: ");
         String name = scan.next();
-        System.out.println("Hello I am " + name);
+        System.out.print("What gender pronouns does " + name +" use?: ");
+        String pronoun = scan.next();
+        System.out.println("Hello I am " + name + " my pronouns are " + pronoun);
+
     }
 }
