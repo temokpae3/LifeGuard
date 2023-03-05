d4e7699a0e7e66820fff0a8e5f6f13f9f98f3a22
TemitopeEmokpae <temokpae@oswego.edu>
2023-03-05 09:28:27 -0500

    added the water and the girl

--- front_end/lifeguard-app/src/Start.js
+++ front_end/lifeguard-app/src/Start.js
@@ -7,7 +7,7 @@ import character3 from "./character3.png";
 import character4 from "./character4.png";
 import character5 from "./character5.png";
 import character6 from "./character6.png";
-
+import water_1 from "./1_7 water.jpg"
 
 function Start() {
   return (
@@ -17,8 +17,12 @@ function Start() {
             style={{height: 700 }}
             src={Computer_Screen} />
             <img
-                style={{height: 300, transform: 'translate(0%, -230%)'}}
-                src={character3}
+                style={{height: 290, transform: 'translate(0%, -230%)'}}
+                src={character6}
+            />
+            <img
+                style={{height: 71.5, transform: 'translate(0%, -880%)'}}
+                src={water_1}
             />
     </header>
     </div>
